ee5db651528280a0701403c9403378c8891db0df
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-07-09 12:25:56 +0530

    2nd commit
    added marks for students

--- Oops/member_fxn.cpp
+++ Oops/member_fxn.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Student{
 public:
        string name;
+       int marks;
        int age;
 
     //    void display(){
@@ -14,13 +15,14 @@ public:
 
 };
 void Student::display(){        //member function ko class ke outside krne ke liye class me declare krna pega 
-    cout<<name<<" "<<age<<endl;
+    cout<<name<<" "<<age<<" "<<marks<<endl;
 }
 
 int main(){
     Student s1;
     s1.name = "amit";
     s1.age = 20;
+    s1.marks = 100;
     s1.display();
     return 0 ;
 }
