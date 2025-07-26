818e3605b6e0dbf0370ddbba9e25b87914b3986d
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-07-26 17:48:19 +0530

    2sum

--- Arrays (MEDIUM)/2SUM_.two.cpp
+++ Arrays (MEDIUM)/2SUM_.two.cpp
@@ -3,19 +3,7 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums){
-    int target =5;
-    for(int i = 0 ; i<nums.size() ; i++){
-        for(int j = i + 1 ; j <nums.size();j++){
-
-            if(nums[i]+nums[j] == target){
-                return {i , j};
-            }
-            
-
-        }
-        
-    }
-    return {};
+   
 }
 
 int main(){
