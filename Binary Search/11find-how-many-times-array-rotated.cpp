294346e47922c559d15a7817bc660f03e46659c2
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-08-06 22:50:36 +0530

    find minimum

---
+++ Binary Search/11find-how-many-times-array-rotated.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findmin(vector<int>& nums){
+    int st=0;
+    int end = nums.size()-1;
+    while(st<end){
+        int mid = st +(end-st)/2;
+        if(nums[mid]>nums[end]){
+            st=mid+1;
+        }
+        else{
+            end = mid;
+        }
+    }
+    return st;
+}
+int main(){
+    vector<int> nums ={3,4,5,1};
+    cout<<findmin(nums);
+    return 0;
+}
