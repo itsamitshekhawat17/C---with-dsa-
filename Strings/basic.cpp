876a005f8816ba973390307241df8e82574830da
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-08-07 23:38:55 +0530

    strings started

---
+++ Strings/basic.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    // string str = "Amit";
+
+    // string str1("Singh");
+
+    // cout<<str<<" "<<str1;
+
+    string str;
+    getline(cin,str);
+    cout<<str;
+
+    return 0;
+}
