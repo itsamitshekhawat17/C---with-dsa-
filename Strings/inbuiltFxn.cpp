70b22817f3a8f34015656be3db0e0d0e54fa0af8
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-08-09 17:13:52 +0530

    did built in fxn and one ques

---
+++ Strings/inbuiltFxn.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+int main(){
+    // string str  = "Amit";
+    // string str1 = "Singh";
+    // reverse(str.begin(),str.end());   //Reverse of a string 
+    // cout<<str<<endl;
+
+    // cout<<str.substr(0,3)<<endl;    //sub string 
+    // return 0;
+
+
+    // cout<<str+str1<<endl;      //concatenation of string 
+
+    // char s1[20]= "amit";
+    // char s2[20]= "singh";
+    // strcat(s1,s2);
+    // cout<<s1<<endl;
+
+    // string str = "abcd";
+    // char ch = 'e';
+    // str.push_back(ch);
+    // cout<<str<<endl;
+
+
+    // string str = "amit";
+    // cout<<str.size()<<endl;
+
+    // char s1[20] = "amit";
+    // cout<<strlen(s1)<<endl;
+
+
+
+    int num = 432;
+    string str = to_string(num);
+    str += "1";
+    cout<<str<<endl;
+
+
+}
