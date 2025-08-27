814b74ad376b103f050a0dfa0a60d251f972425f
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-08-27 23:16:17 +0530

    Add initial implementation of Node class for linked list

---
+++ LinkedList/ll.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = NULL;
+
+    }
+};
