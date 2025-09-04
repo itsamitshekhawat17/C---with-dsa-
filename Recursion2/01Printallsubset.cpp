e31d6d7d4c5ddc7aa828aec17819895ff1dbe792
itsamitshekhawat17 <itsmeamitsingh17@gmail.com>
2025-09-04 18:55:38 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Recursion2/01Printallsubset.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printsubsets(vector<int> &arr, vector<int> &ans , int i){
+
+    if(i==arr.size()){
+        for(int val:ans){
+            cout<<val<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    //ye to inclusion wala step ho gya 
+    ans.push_back(arr[i]);
+    printsubsets(arr,ans,i+1);
+
+    ans.pop_back();
+    // Exclusion
+    printsubsets(arr,ans,i+1);
+
+    
+
+}
+
+int main(){
+    vector<int> arr = {1,2,3};
+    vector<int> ans ;
+
+    printsubsets(arr,ans,0);
+}
